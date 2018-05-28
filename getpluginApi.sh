087ff09b777c1999b33dcb6b3cb6c8cc71615aa5
smsantana <saul.ms@hotmail.com>
2018-05-28 11:11:28 -0300

    Update getpluginApi.sh

--- getpluginApi.sh
+++ getpluginApi.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
-JENKINS_HOST="http://localhost:8080"
+#Alterar as informacoes abaixo
+JENKINS_PROTOCOL="http://"
+JENKINS_HOST="localhost:8080"
 JENKINS_USER="admin"
 JENKINS_PASS="admin123"
 
+#Nao alterar a partir daqui
+JENKINS_URL="${JENKINS_PROTOCOL}${JENKINS_USER}:${JENKINS_PASS}@${JENKINS_HOST}"
+
 echo "Get Jenkins CLI"
-curl "${JENKINS_HOST}/jnlpJars/jenkins-cli.jar" > jenkins-cli.jar
+curl "${JENKINS_URL}/jnlpJars/jenkins-cli.jar" > jenkins-cli.jar
 echo;
 
 echo "Get PLugins Instaled Stance: ${JENKINS_HOST}"
 echo;
 
-java -jar jenkins-cli.jar -s ${JENKINS_USER}:${JENKINS_PASS}@${JENKINS_HOST} groovy = < InventoryPlugins.groovy > plugins.txt
+java -jar jenkins-cli.jar -s ${JENKINS_URL} groovy = < InventoryPlugins.groovy > plugins.txt
 
 echo;
 cat plugins.txt
